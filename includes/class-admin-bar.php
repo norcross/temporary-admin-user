f959ccd70a62df1cb110aa345eff93194a4b4b90
Norcross <andrew.norcross@gmail.com>
2018-02-02 11:14:19 -0500

    adding the admin bar function and the file load.

---
+++ includes/class-admin-bar.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Our new admin bar item.
+ *
+ * @package TempAdminUser
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+add_action( 'admin_bar_menu', 'tmp_admin_user_admin_bar', 9999 );
+/**
+ * Add a menu item under the "new content" for new users.
+ *
+ * @param  WP_Admin_Bar $wp_admin_bar  The global WP_Admin_Bar object.
+ *
+ * @return void.
+ */
+function tmp_admin_user_admin_bar( WP_Admin_Bar $wp_admin_bar ) {
+
+	// Add the link to the menu page.
+	$wp_admin_bar->add_node(
+		array(
+			'id'        => 'tmp-admin-user-new',
+			'title'     => __( 'Temporary Admin', 'temporary-admin-user' ),
+			'href'      => TempAdminUser_Helper::get_menu_link(),
+			'parent'    => 'new-content',
+			'meta'      => array(
+				'title' => __( 'Temporary Admin', 'temporary-admin-user' ),
+			),
+		)
+	);
+}
